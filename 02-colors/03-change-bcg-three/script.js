6b3e2ebdf700cc2192f0033a6bab19e459b147a4
Besart Elezi <besart.elezi@hotmail.com>
2022-05-05 16:38:39 +0200

    finished the last assignment of the colors folder

--- 02-colors/03-change-bcg-three/script.js
+++ 02-colors/03-change-bcg-three/script.js
@@ -12,5 +12,17 @@
 (function() {
 
     // your code here
+    const click = document.getElementById("run");
+    console.log(click)
+    click.addEventListener("click", random_bg_color)
+    function random_bg_color() {
+        var x = Math.floor(Math.random() * 256);
+        var y = Math.floor(Math.random() * 256);
+        var z = Math.floor(Math.random() * 256);
+        var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+        console.log(bgColor);
+
+        document.body.style.background = bgColor;
+    }
 
 })();
