1c5a9af12090280394f245faa1f929839d79c42c
Besart Elezi <besart.elezi@hotmail.com>
2022-05-13 14:56:17 +0200

    started on optional 01/01, I have put every letter from the targeted text into an array all as seperate objects

--- optional/01-misc/01-waving-text/script.js
+++ optional/01-misc/01-waving-text/script.js
@@ -12,5 +12,15 @@
 (function() {
 
     // your code here
+    // This selects the target text, and then splits every letter from the text into a seperate object in an array
+    const Target = document.getElementById("target");
+    const StringTarget = Target.textContent;
+    const SplitText = StringTarget.split ("");
+    Target.textContent = ""
 
+    // This puts a <span> element between every character from the string
+    for (let i=0; i < SplitText.length; i++){
+        Target.innerHTML += "<span>" + SplitText[i] + "</span>";
+    }
+    
 })();
